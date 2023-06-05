cfd8fa933118a9b62f137bf5f685d4262252c174
Oleg <ov.chilikhin2022@yandex.ru>
2023-06-05 11:02:41 +0400

    Remove default files from project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+const root = document.querySelector("#root") as HTMLElement;
+
+createRoot(root).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 3000,
+    host: true,
+    proxy: {
+      "/api": "http://localhost:4200/",
+    },
+  },
+  resolve: {
+    alias: {
+      "~styles": resolve(__dirname, "src/assets/styles"),
+      "~fonts": resolve(__dirname, "src/assets/fonts"),
+    },
+  },
+
+  plugins: [tsconfigPaths(), react()],
+});
